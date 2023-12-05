7d2c99279e5d6ac80a42fc9686ef3cbaa4597463
dani-day <allkite5122@gmail.com>
2023-12-05 11:43:29 +0900

    ✨ [Feat] : History 페이지의 연혁 카드 기능 추가
    History 페이지의 연혁 카드 기능을 추가하였습니다.
    + 연혁 Swiper 와 카드 Swiper 를 추가하였습니다.
    + 연혁 Swiper 와 카드 Swiper 가 연동되어 움직이도록 하였습니다.
    + 첫번째 카드가 가운데 오도록 하였으며, 연혁 Swiper 의 slidesPerView 를 더 많이 보이도록 설정하였습니다.
    + 연혁 Swiper 는 isActive 여부에 따라 다른 마크업 및 스타일링이 적용됩니다. (컬러, 폰트 사이즈)
    + 기본 Markup 과 스타일링을 완료하였습니다.
    
    ! 내용은 dummy 로 넣어두었습니다.
    ! 카드 스타일링이 완료되지 않았습니다. (카드 width 조정 필요, 항목별 왼쪽 원형 아이콘 추가 필요)

--- src/views/History.jsx
+++ src/views/History.jsx
@@ -1,7 +1,200 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Controller, EffectCoverflow } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
 
 function History() {
-  return <div>History</div>;
-}
+  // controller 설정을 위한 상태입니다.
+  const [yearSwiper, setYearSwiper] = useState(null);
+  const [cardSwiper, setCardSwiper] = useState(null);
+  return (
+    <section className="w-full h-full flex flex-col px-[300px] font-open-font items-center justify-center -bg--openfoundation-primary">
+      <div className="historyWrapper w-[1320px] flex flex-col gap-[100px] -text--openfoundation-secondary font-open-label -bg--openfoundation-primary">
+        <Swiper
+          centeredSlides
+          modules={[Controller, EffectCoverflow]}
+          effect="coverflow"
+          slidesPerView={7}
+          coverflowEffect={{
+            rotate: 0,
+            stretch: 0,
+            depth: 0,
+            modifier: 0,
+            slideShadows: true,
+          }}
+          onSwiper={setYearSwiper}
+          controller={{ by: 'container', control: cardSwiper }}
+          className="w-full h-[200px] text-open-font-xxxxl text-center flex flex-col justify-center items-center"
+        >
+          <SwiperSlide className="flex flex-col justify-end items-center gap-[20px]">
+            {({ isActive }) => (
+              isActive ? (
+                <>
+                  <div>2023</div>
+                  <img src="/src/assets/history_circle.svg" alt="" aria-hidden />
+                </>
+              ) : (
+                <>
+                  <div className="text-open-font-xxl -text--open-gray-300">2023</div>
+                  <img src="/src/assets/history_circleline.svg" alt="" aria-hidden />
+                </>
+              )
+            )}
+          </SwiperSlide>
+          <SwiperSlide className="flex flex-col justify-end items-center gap-[20px]">
+            {({ isActive }) => (
+              isActive ? (
+                <>
+                  <div>2022</div>
+                  <img src="/src/assets/history_circle.svg" alt="" aria-hidden />
+                </>
+              ) : (
+                <>
+                  <div className="text-open-font-xxl -text--open-gray-300">2022</div>
+                  <img src="/src/assets/history_circleline.svg" alt="" aria-hidden />
+                </>
+              )
+            )}
+          </SwiperSlide>
+          <SwiperSlide className="flex flex-col justify-end items-center gap-[20px]">
+            {({ isActive }) => (
+              isActive ? (
+                <>
+                  <div>2021</div>
+                  <img src="/src/assets/history_circle.svg" alt="" aria-hidden />
+                </>
+              ) : (
+                <>
+                  <div className="text-open-font-xxl -text--open-gray-300">2021</div>
+                  <img src="/src/assets/history_circleline.svg" alt="" aria-hidden />
+                </>
+              )
+            )}
+          </SwiperSlide>
+          <SwiperSlide className="flex flex-col justify-end items-center gap-[20px]">
+            {({ isActive }) => (
+              isActive ? (
+                <>
+                  <div>2020</div>
+                  <img src="/src/assets/history_circle.svg" alt="" aria-hidden />
+                </>
+              ) : (
+                <>
+                  <div className="text-open-font-xxl -text--open-gray-300">2020</div>
+                  <img src="/src/assets/history_circleline.svg" alt="" aria-hidden />
+                </>
+              )
+            )}
+          </SwiperSlide>
+          <SwiperSlide className="flex flex-col justify-end items-center gap-[20px]">
+            {({ isActive }) => (
+              isActive ? (
+                <>
+                  <div>2019</div>
+                  <img src="/src/assets/history_circle.svg" alt="" aria-hidden />
+                </>
+              ) : (
+                <>
+                  <div className="text-open-font-xxl -text--open-gray-300">2019</div>
+                  <img src="/src/assets/history_circleline.svg" alt="" aria-hidden />
+                </>
+              )
+            )}
+          </SwiperSlide>
+          <SwiperSlide className="flex flex-col justify-end items-center gap-[20px]">
+            {({ isActive }) => (
+              isActive ? (
+                <>
+                  <div>2018</div>
+                  <img src="/src/assets/history_circle.svg" alt="" aria-hidden />
+                </>
+              ) : (
+                <>
+                  <div className="text-open-font-xxl -text--open-gray-300">2018</div>
+                  <img src="/src/assets/history_circleline.svg" alt="" aria-hidden />
+                </>
+              )
+            )}
+          </SwiperSlide>
+        </Swiper>
 
+        <Swiper
+          centeredSlides
+          modules={[Controller, EffectCoverflow]}
+          effect="coverflow"
+          slidesPerView={3}
+          coverflowEffect={{
+            rotate: 0,
+            stretch: 50,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+            scale: 0.7,
+          }}
+          onSwiper={setCardSwiper}
+          controller={{ by: 'container', control: yearSwiper }}
+          className="w-full h-full"
+        >
+          <SwiperSlide className="h-[582px] py-[120px] px-[40px] -bg--open-gray-950 rounded-[20px]">
+            <div className="flex flex-col items-center justify-center gap-9">
+              <span className="text-open-font-xxxxxl">2023</span>
+              <ul className="font-open-paragraph text-open-font-xl flex flex-col gap-[20px]">
+                <li>경영혁신형 중소기업 Main-biz 인증 획득</li>
+                <li>경광등 작동신호 무선 송출기 발명 특허 등록</li>
+              </ul>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="h-[582px] py-[120px] px-[40px] -bg--open-gray-950 rounded-[20px]">
+            <div className="flex flex-col items-center justify-center gap-9">
+              <span className="text-open-font-xxxxxl">2022</span>
+              <ul className="font-open-paragraph text-open-font-xl flex flex-col gap-[20px]">
+                <li>경영혁신형 중소기업 Main-biz 인증 획득</li>
+                <li>경광등 작동신호 무선 송출기 발명 특허 등록</li>
+              </ul>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="h-[582px] py-[120px] px-[40px] -bg--open-gray-950 rounded-[20px]">
+            <div className="flex flex-col items-center justify-center gap-9">
+              <span className="text-open-font-xxxxxl">2021</span>
+              <ul className="font-open-paragraph text-open-font-xl flex flex-col gap-[20px]">
+                <li>경영혁신형 중소기업 Main-biz 인증 획득</li>
+                <li>경광등 작동신호 무선 송출기 발명 특허 등록</li>
+              </ul>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="h-[582px] py-[120px] px-[40px] -bg--open-gray-950 rounded-[20px]">
+            <div className="flex flex-col items-center justify-center gap-9">
+              <span className="text-open-font-xxxxxl">2020</span>
+              <ul className="font-open-paragraph text-open-font-xl flex flex-col gap-[20px]">
+                <li>경영혁신형 중소기업 Main-biz 인증 획득</li>
+                <li>경광등 작동신호 무선 송출기 발명 특허 등록</li>
+              </ul>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="h-[582px] py-[120px] px-[40px] -bg--open-gray-950 rounded-[20px]">
+            <div className="flex flex-col items-center justify-center gap-9">
+              <span className="text-open-font-xxxxxl">2019</span>
+              <ul className="font-open-paragraph text-open-font-xl flex flex-col gap-[20px]">
+                <li>경영혁신형 중소기업 Main-biz 인증 획득</li>
+                <li>경광등 작동신호 무선 송출기 발명 특허 등록</li>
+              </ul>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="h-[582px] py-[120px] px-[40px] -bg--open-gray-950 rounded-[20px]">
+            <div className="flex flex-col items-center justify-center gap-9">
+              <span className="text-open-font-xxxxxl">2018</span>
+              <ul className="font-open-paragraph text-open-font-xl flex flex-col gap-[20px]">
+                <li>경영혁신형 중소기업 Main-biz 인증 획득</li>
+                <li>경광등 작동신호 무선 송출기 발명 특허 등록</li>
+              </ul>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </section>
+
+  );
+}
 export default History;
