b0f4513056357cbab28d5a6ab3d9aeeb5d63965f
ryudg <ryudg95@gmail.com>
2023-12-07 11:37:36 +0900

    ✨ [Feat] : 관련 제품 링크 추가 - Solution 별 링크 추가   - 해당 Solution 페이지로 이동
    Github issue #16

--- src/components/Industries/Product.jsx
+++ src/components/Industries/Product.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
+import { Link } from 'react-router-dom';
 
 function Product({ industryProducts, refs }) {
   return (
@@ -14,25 +15,24 @@ function Product({ industryProducts, refs }) {
       </h3>
       <div className="flex flex-col desktop:gap-open-2xl tablet:gap-open-2xl gap-mobile-gap">
         {Object.entries(industryProducts).map((item) => (
-          <div
-            key={uuidv4()}
-            className="flex items-start desktop:gap-desktop-gap tablet:gap-desktop-gap gap-mobile-gap"
-          >
-            <div className="desktop:w-open-7xl tablet:w-open-7xl w-open-5xl">
-              <img src="http://via.placeholder.com/200x250" alt="제픔 사진" className="w-full" />
+          <Link key={uuidv4()} to={`/solutions/${item[0]}`}>
+            <div className="flex items-start desktop:gap-desktop-gap tablet:gap-desktop-gap gap-mobile-gap hover:underline">
+              <div className="desktop:w-open-7xl tablet:w-open-7xl w-open-5xl">
+                <img src="http://via.placeholder.com/200x250" alt="제픔 사진" className="w-full" />
+              </div>
+              <ul className="flex-1 desktop:px-open-xl tablet:px-open-xl desktop:py-open-xl tablet:py-open-xl py-open-lg px-open-lg flex flex-col gap-open-xl">
+                <li className="mb-[-20px] font-open-heading desktop:text-open-font-xxl tablet:text-open-font-xxl text-open-font-large">
+                  {item[0]}
+                </li>
+                <li className="font-open-label desktop:text-open-font-xl tablet:text-open-font-xl text-open-font-medium">
+                  {item[1].제품명}
+                </li>
+                <li className="font-open-paragraph desktop:text-open-font-large tablet:text-open-font-large text-open-font-medium">
+                  {item[1].제품설명}
+                </li>
+              </ul>
             </div>
-            <ul className="flex-1 desktop:px-open-xl tablet:px-open-xl desktop:py-open-xl tablet:py-open-xl py-open-lg px-open-lg flex flex-col gap-open-xl">
-              <li className="mb-[-20px] font-open-heading desktop:text-open-font-xxl tablet:text-open-font-xxl text-open-font-large">
-                {item[0]}
-              </li>
-              <li className="font-open-label desktop:text-open-font-xl tablet:text-open-font-xl text-open-font-medium">
-                {item[1].제품명}
-              </li>
-              <li className="font-open-paragraph desktop:text-open-font-large tablet:text-open-font-large text-open-font-medium">
-                {item[1].제품설명}
-              </li>
-            </ul>
-          </div>
+          </Link>
         ))}
       </div>
     </section>
