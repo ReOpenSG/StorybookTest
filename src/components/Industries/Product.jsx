b0e2b02afb0d8fddd38e5e9457290d3951d9bb7d
ryudg <ryudg95@gmail.com>
2023-12-07 10:50:06 +0900

    ✨ [Feat] : Product 컴포넌트 작성 - 산업군에 관련된 제픔을 설명하는 컴포넌트 작성   - solutionData.json에 작성되어있는 솔루션의 산업군을,     industriesData.js의 각 산업군별 solutions 배열에 매칭시켜 추가 후     industriesData.js에 작성되어있는 산업군의 이름과 해당 주소를 비교하여 일치하는 솔루션을 렌더링
    Github issue #16

---
+++ src/components/Industries/Product.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { v4 as uuidv4 } from 'uuid';
+
+function Product({ industryProducts, refs }) {
+  return (
+    <section
+      id="Product"
+      ref={refs}
+      className="flex flex-col desktop:gap-open-2xl tablet:desktop:gap-open-2xl gap-open-xl desktop:pt-open-5xl tablet:pt-open-5xl pt-open-3xl"
+    >
+      <h3 className="font-open-label desktop:text-open-font-xxl tablet:text-open-font-xxl text-open-font-large">
+        관련 제품
+      </h3>
+      <div className="flex flex-col desktop:gap-open-2xl tablet:gap-open-2xl gap-mobile-gap">
+        {Object.entries(industryProducts).map((item) => (
+          <div
+            key={uuidv4()}
+            className="flex items-start desktop:gap-desktop-gap tablet:gap-desktop-gap gap-mobile-gap"
+          >
+            <div className="desktop:w-open-7xl tablet:w-open-7xl w-open-5xl">
+              <img src="http://via.placeholder.com/200x250" alt="제픔 사진" className="w-full" />
+            </div>
+            <ul className="flex-1 desktop:px-open-xl tablet:px-open-xl desktop:py-open-xl tablet:py-open-xl py-open-lg px-open-lg flex flex-col gap-open-xl">
+              <li className="mb-[-20px] font-open-heading desktop:text-open-font-xxl tablet:text-open-font-xxl text-open-font-large">
+                {item[0]}
+              </li>
+              <li className="font-open-label desktop:text-open-font-xl tablet:text-open-font-xl text-open-font-medium">
+                {item[1].제품명}
+              </li>
+              <li className="font-open-paragraph desktop:text-open-font-large tablet:text-open-font-large text-open-font-medium">
+                {item[1].제품설명}
+              </li>
+            </ul>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+Product.propTypes = {
+  industryProducts: PropTypes.oneOfType([PropTypes.object]).isRequired,
+  refs: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.shape({ current: PropTypes.instanceOf(Element) }),
+  ]).isRequired,
+};
+
+export default Product;
