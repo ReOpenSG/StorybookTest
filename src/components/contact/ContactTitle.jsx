75010efc87d17e61995bc5b09bcb676327274873
ingbinsee <ingbinsee@gmail.com>
2023-12-08 14:31:50 +0900

    ✨ [Feat] : Contact 페이지 Title 컴포넌트 완료
    Title 컴포넌트를 구현하였습니다.
    + 분기점(모바일, 태블릿, 데스크탑)에 따른 변화를 반영하였습니다.
    
    Related to: #28

---
+++ src/components/contact/ContactTitle.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function ContactTitle() {
+  return (
+    <div className="flex flex-col gap-open-xl tablet:gap-open-2xl desktop:gap-open-2xl text-center my-[60px] tablet:my-open-5xl desktop:my-open-5xl">
+      <h2 className="-text--openfoundation-primary font-open-label text-open-font-large">
+        Support
+      </h2>
+      <h3 className="-text--openfoundation-primary font-open-heading text-open-font-xxl">
+        고객문의
+      </h3>
+      <p className="-text--openfoundation-primary font-open-paragraph text-open-font-medium">
+        상세하게 작성해 주시면 더 빠르게 답변드릴 수 있습니다.
+      </p>
+    </div>
+  );
+}
+
+export default ContactTitle;
