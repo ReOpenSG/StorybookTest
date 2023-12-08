7dd4305d48512c7e2558b906579be801295ee6ae
Chi Hyun Noh <101504272+JuniorTunarr@users.noreply.github.com>
2023-12-08 11:00:34 +0900

    ✨ [Feat] : Admin 기능 추가
    1. admin 페이지를 만들었습니다.
    2. 게시판(커뮤니티)에 글을 CRUD하기 위하여,
    firebase의 auth 계정 생성후 설정하였습니다.
    3. admin 계정 로그인 여부와 인증 유지를 전역에서 상태관리 하기 위하여,
    recoil로 auth를 추적 관리하게 설정하였습니다.
    
    Related to #1 #4

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
-import RootLayout from '../layout/RootLayout';
-import About from '../views/About';
-import Home from '../views/Home';
-import History from '../views/History';
-import Team from '../views/Team';
-import Location from '../views/Location';
-import Solutions from '../views/Solutions';
-import Machines from '../views/Machines';
-import Library from '../views/Library';
-import Industries from '../views/Industries';
-import Contact from '../views/Contact';
-import Device from '../views/Device';
-import Community from '../views/Community';
+import RootLayout from '@/layout/RootLayout';
+import About from '@/views/About';
+import Home from '@/views/Home';
+import History from '@/views/History';
+import Team from '@/views/Team';
+import Location from '@/views/Location';
+import Solutions from '@/views/Solutions';
+import Machines from '@/views/Machines';
+import Library from '@/views/Library';
+import Industries from '@/views/Industries';
+import Contact from '@/views/Contact';
+import Device from '@/views/Device';
+import Community from '@/views/Community';
 import CommunityDetail from '../views/CommunityDetail';
+import Admin from '@/views/Admin';
+import CommunityForm from '@/views/CommunityForm';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -31,6 +33,8 @@ const router = createBrowserRouter(
       <Route path="/contact" element={<Contact />} />
       <Route path="/community" element={<Community />} />
       <Route path="/community/:id" element={<CommunityDetail />} />
+      <Route path="/community/write" element={<CommunityForm />} />
+      <Route path="/admin" element={<Admin />} />
     </Route>,
   ),
 );
--- src/views/Admin.jsx
+++ src/views/Admin.jsx
@@ -1,22 +1,41 @@
-import React, { useState } from 'react';
+import {
+  browserSessionPersistence,
+  setPersistence,
+  signInWithEmailAndPassword,
+  signOut,
+} from 'firebase/auth';
+import React, { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import { useRecoilValue } from 'recoil';
 import { useNavigate } from 'react-router-dom';
+import { authService } from '../../firebase';
+import { isLoggedInState } from '@/recoil/atoms/authStore';
 
 function Admin() {
   const [id, setId] = useState('');
   const [pw, setPw] = useState('');
-
+  const isLoggedIn = useRecoilValue(isLoggedInState);
   const navigate = useNavigate();
   const handleId = (e) => setId(e.target.value);
   const handlePw = (e) => setPw(e.target.value);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    if (id === 'opensg' && pw === 'opensgBest!') {
+    try {
+      await signInWithEmailAndPassword(authService, id, pw);
+      await setPersistence(authService, browserSessionPersistence);
       navigate(-1);
-    } else {
-      alert('아이디 또는 비밀번호가 올바르지 않습니다.');
+    } catch (error) {
+      toast(error);
     }
   };
+
+  const handleLogout = () => {
+    signOut(authService);
+    setId('');
+    setPw('');
+  };
+
   return (
     <div className="flex flex-col w-[1440px] min-w-[1440px] max-width-[1920px] justify-center items-center gap-[120px]">
       <section className="w-full py-20 flex flex-col text-center gap-8">
@@ -24,39 +43,55 @@ function Admin() {
         <h2 className="text-open-font-xxxxl font-bold">관리자 페이지</h2>
       </section>
       <section className="w-full flex flex-col justify-center items-center gap-8">
-        <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center gap-10">
-          <div className="flex gap-2.5">
-            <label htmlFor="id" className="w-[100px] text-open-font-xl">
-              아이디
-            </label>
+        {!isLoggedIn ? (
+          <form
+            onSubmit={handleSubmit}
+            className="flex flex-col justify-center items-center gap-10"
+          >
+            <div className="flex gap-2.5">
+              <label htmlFor="id" className="w-[100px] text-open-font-xl">
+                아이디
+              </label>
+              <input
+                type="text"
+                id="id"
+                name="id"
+                onChange={handleId}
+                placeholder="아이디를 입력하세요"
+                className="p-2.5 w-[240px] border -border--open-gray-300 rounded"
+              />
+            </div>
+            <div className="flex gap-2.5">
+              <label htmlFor="pw" className="w-[100px] text-open-font-xl">
+                비밀번호
+              </label>
+              <input
+                type="password"
+                id="pw"
+                name="pw"
+                onChange={handlePw}
+                placeholder="비밀번호를 입력하세요"
+                className="p-2.5 w-[240px] border -border--open-gray-300 rounded"
+              />
+            </div>
             <input
-              type="text"
-              id="id"
-              name="id"
-              onChange={handleId}
-              placeholder="아이디를 입력하세요"
-              className="p-2.5 w-[240px] border -border--open-gray-300 rounded"
+              type="submit"
+              value="로그인"
+              className="text-open-font-xl text-white rounded-[20px] -bg--open-blue-900 w-[355px] p-2.5 my-10"
             />
+          </form>
+        ) : (
+          <div className="flex flex-col justify-center items-center gap-10">
+            <h3 className="text-open-font-xl">환영합니다, 관리자님!</h3>
+            <button
+              type="button"
+              onClick={handleLogout}
+              className="text-open-font-xl text-white rounded-[20px] -bg--open-blue-900 w-[355px] p-2.5 my-10"
+            >
+              로그아웃
+            </button>
           </div>
-          <div className="flex gap-2.5">
-            <label htmlFor="pw" className="w-[100px] text-open-font-xl">
-              비밀번호
-            </label>
-            <input
-              type="password"
-              id="pw"
-              name="pw"
-              onChange={handlePw}
-              placeholder="비밀번호를 입력하세요"
-              className="p-2.5 w-[240px] border -border--open-gray-300 rounded"
-            />
-          </div>
-          <input
-            type="submit"
-            value="로그인"
-            className="text-open-font-xl text-white rounded-[20px] -bg--open-blue-900 w-[355px] p-2.5 my-10"
-          />
-        </form>
+        )}
       </section>
     </div>
   );
