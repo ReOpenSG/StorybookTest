778f8c295f7949e331f95eebfafb7f682db740ba
ingbinsee <ingbinsee@gmail.com>
2023-12-10 17:22:07 +0900

    ⚙️ [Chore] : 공통 환경 세팅 변경
    + ToastContainer를 RootLayout에서 1회 사용하여 공통으로 활용할 수 있도록 하였습니다.
    + tailwind.config.js에서 spacing의 변경된 크기를 반영하였습니다.

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import Header from './Header';
+import 'react-toastify/dist/ReactToastify.css';
 
 function RootLayout() {
   return (
@@ -9,6 +11,17 @@ function RootLayout() {
       <main className="flex justify-center mx-auto font-open-font pt-[96px]">
         <Outlet />
       </main>
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </>
   );
 }
