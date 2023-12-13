fb8d9fc3fd289d4bf38920761445db1202d08c2b
ingbinsee <ingbinsee@gmail.com>
2023-12-13 11:33:01 +0900

    ♻️ [Refactor] : 마크업 수정
    물리적 주소, 연락처, 팩스번호 등 연락처 정보에 대한 내용을 <div>에서 <adress>로 마크업을 수정하였습니다.
    
    Related to: #46

--- src/components/Footer/OfficeInfoSection.jsx
+++ src/components/Footer/OfficeInfoSection.jsx
@@ -5,13 +5,13 @@ import styles from '@/components/Footer/Footer.module.css';
 function OfficeInfoSection() {
   return (
     <section className={styles.officeWrapper}>
-      <div className={`${styles.officeInfo} ${styles.officeText}`}>
+      <address className={`${styles.officeInfo} ${styles.officeText}`}>
         <span>경기도 화성시 동탄첨단산업1로 51-9 M타워 1201호</span>
         <div className="flex gap-open-gutter-mobile tablet:gap-open-gutter-desktop desktop:gap-open-gutter-desktop">
           <Link to="tel:031-373-2612">T. 031-373-2612</Link>
           <span>F. 031-377-2615</span>
         </div>
-      </div>
+      </address>
       <small className={`${styles.officeCopyright} ${styles.officeText}`}>
         Copyright &copy;OpenSG, All rights reserved.
       </small>
