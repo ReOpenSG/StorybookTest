bc3fafbc588a8645c7a4038d30bca518482a2a35
Chi Hyun Noh <101504272+JuniorTunarr@users.noreply.github.com>
2023-12-15 12:49:16 +0900

    ✨ [Feat] : Industry section 라우팅 연결
    메인 페이지 industry 섹션에서 Industry 컴포넌트로 라우팅을 연결하였습니다.
    
    Related to #50

--- src/components/Home/Industries.jsx
+++ src/components/Home/Industries.jsx
@@ -1,6 +1,35 @@
 import React from 'react';
+import { v4 as uuid } from 'uuid';
+import { Link } from 'react-router-dom';
+import Image1 from '@/assets/home_industry1.png';
 
 function Industries() {
+  const industryData = [
+    {
+      id: uuid(),
+      img: Image1,
+      title: 'Secondary Battery',
+      route: 'battery',
+    },
+    {
+      id: uuid(),
+      img: Image1,
+      title: 'Display',
+      route: 'display',
+    },
+    {
+      id: uuid(),
+      img: Image1,
+      title: 'Port',
+      route: 'port',
+    },
+    {
+      id: uuid(),
+      img: Image1,
+      title: 'Semi-conductor',
+      route: 'semiconductor',
+    },
+  ];
   return (
     <section className="flex flex-col ml-auto gap-open-3xl tablet:gap-open-5xl desktop:gap-open-5xl py-open-5xl tablet:py-open-7xl desktop:py-open-7xl pl-open-gutter-mobile tablet:pl-open-margin-desktop desktop:pl-open-8xl">
       <div className="flex flex-col gap-open-3xl tablet:gap-open-5xl desktop:gap-open-5xl">
@@ -21,7 +50,27 @@ function Industries() {
         </div>
       </div>
 
-      <div className="h-[288px] tablet:h-[650px] desktop:h-[650px] w-full bg-black" />
+      <div className="w-full flex flex-col justify-center h-[288px] tablet:h-[650px] desktop:h-[650px] bg-black rounded-l-[30px] overflow-hidden">
+        <div className="w-full h-fit flex py-open-xl pl-open-xl gap-open-gutter-desktop tablet:py-open-2xl desktop:py-open-2xl tablet:pl-open-5xl desktop:pl-open-5xl tablet:gap-open-margin-desktop desktop:gap-open-margin-desktop">
+          {industryData.map((item) => (
+            <Link to={`/industries/${item.route}`}>
+              <div
+                key={item.id}
+                className=" flex w-open-6xl tablet:w-open-8xl desktop:w-open-8xl gap-open-lg tablet:gap-open-lg desktop:gap-open-lg flex-col items-center transform opacity-80 active:opacity-100 transition-all duration-300 hover:opacity-100 hover:skew-y-0 hover:scale-105 hover:z-10"
+              >
+                <img
+                  src={item.img}
+                  alt="industries"
+                  className="w-open-6xl tablet:w-open-8xl  desktop:w-open-8xl"
+                />
+                <p className="font-open-label text-open-font-small tablet:text-open-font-xl desktop:text-open-font-xl -text--openfoundation-white pt-4 tablet:pt-open-xl desktop:pt-open-xl break-words">
+                  {item.title}
+                </p>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
     </section>
   );
 }
