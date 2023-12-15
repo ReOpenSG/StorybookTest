b95489e824aca7dbcc2e5dfb2927ba0228760538
dani-day <allkite5122@gmail.com>
2023-12-15 09:38:15 +0900

    ✨ [Feat] : 지도 새창 링크 기능 및 아이콘 추가
    + 주소 별 새창 링크를 연결하였습니다.
    
    Related to: #61

--- src/components/Location/LocationContainer.jsx
+++ src/components/Location/LocationContainer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import defaultImage from '@/assets/location_defaultImage.png';
+import linkIcon from '@/assets/location_link.svg';
 
 function LocationContainer({
   locationName,
@@ -8,22 +9,30 @@ function LocationContainer({
   locationTel,
   locationFax,
   locationImage,
+  locationLink,
 }) {
   return (
     <div className="locationWrapper flex flex-row justify-center items-center gap-open-gutter-mobile desktop:gap-open-gutter-desktop tablet:gap-open-gutter-desktop pt-open-5xl desktop:pt-open-6xl tablet:pt-open-6xl">
       <div><img className="desktop:w-[400px] desktop:h-[400px] tablet:w-[400px] tablet:h-[400px] w-[200px] h-[200px] object-cover" src={locationImage} alt="본사 전경" /></div>
-      <address className="desktop:w-[500px] tablet:w-[500px] w-[250px] flex flex-col gap-open-md desktop:gap-open-xl tablet:gap-open-xl desktop:text-open-font-large desktop:font-open-label tablet:text-open-font-large tablet:font-open-label text-open-font-medium font-open-paragraph not-italic">
-        <p className="desktop:text-open-font-xl desktop:font-open-heading tablet:text-open-font-xl tablet:font-open-heading text-open-font-medium font-open-label">{locationName}</p>
-        <p>{locationAddress}</p>
-        <div className="flex flex-col gap-open-md">
-          <p>
-            Tel.
-            {locationTel}
-          </p>
-          <p>
-            Fax.
-            {locationFax}
-          </p>
+      <address className="desktop:w-[500px] tablet:w-[500px] w-[250px] flex flex-col justify-between h-full desktop:gap-open-xl tablet:gap-open-xl desktop:text-open-font-large desktop:font-open-label tablet:text-open-font-large tablet:font-open-label text-open-font-medium font-open-paragraph not-italic desktop:py-open-gutter-desktop tablet:py-open-gutter-desktop py-open-gutter-mobile">
+        <div className="informationWrapper flex flex-col gap-open-md">
+          <p className="desktop:text-open-font-xl desktop:font-open-heading tablet:text-open-font-xl tablet:font-open-heading text-open-font-medium font-open-label">{locationName}</p>
+          <p>{locationAddress}</p>
+          <div className="flex flex-col gap-open-md">
+            <p>
+              Tel. &nbsp;
+              {locationTel}
+            </p>
+            <p>
+              Fax. &nbsp;
+              {locationFax}
+            </p>
+          </div>
+        </div>
+        <div className="linkWrapper">
+          <a href={locationLink} target="_blank" rel="noreferrer">
+            <img className="w-[32px] h-[32px] desktop:w-[64px] desktop:h-[64px] tablet:w-[64px] tablet:h-[64px]" src={linkIcon} alt="지도 링크" />
+          </a>
         </div>
       </address>
     </div>
@@ -36,10 +45,11 @@ LocationContainer.propTypes = {
   locationTel: PropTypes.string.isRequired,
   locationFax: PropTypes.string.isRequired,
   locationImage: PropTypes.string,
+  locationLink: PropTypes.string.isRequired,
 };
 
 LocationContainer.defaultProps = {
   locationImage: defaultImage,
-}
+};
 
 export default LocationContainer;
--- src/views/Location.jsx
+++ src/views/Location.jsx
@@ -13,14 +13,15 @@ function Location() {
         <LocationPinIcon usa />
       </div>
       <section className="mb-open-5xl w-full h-full flex flex-col justify-center items-center desktop:px-open-margin-desktop tablet:px-open-margin-desktop px-open-gutter-mobile -bg--openfoundation-primary -text--openfoundation-secondary">
-        <LocationContainer locationName="본사" locationAddress="우 18469 경기도 화성시 동탄첨단산업1로 51-9 엠타워 1201호" locationTel="031-373-2612" locationFax="031-377-2615" locationImage={mTowerImage} />
+        <LocationContainer locationName="본사" locationAddress="우 18469 경기도 화성시 동탄첨단산업1로 51-9 엠타워 1201호" locationTel="031-373-2612" locationFax="031-377-2615" locationImage={mTowerImage} locationLink="https://maps.app.goo.gl/Lda5XHqKMYWHyQpYA" />
         <LocationContainer
           locationName="자동화 연구소"
           locationAddress="우 31094) 충청남도 천안시 서북구 백석동 1206 천안미래에이스하이테크시티 406호"
           locationTel="041-566-2612"
           locationFax="041-566-2613"
+          locationLink="https://maps.app.goo.gl/qMbgiatVSPKXaVGUA"
         />
-        <LocationContainer locationName="오픈에스지 미국지사" locationAddress="미국 지사 주소가 들어갈 예정입니다." locationTel="미국 지사 전화번호" locationFax="미국 지사 팩스번호" />
+        <LocationContainer locationName="오픈에스지 미국지사" locationAddress="미국 지사 주소가 들어갈 예정입니다." locationTel="미국 지사 전화번호" locationFax="미국 지사 팩스번호" locationLink="https://maps.app.goo.gl/Kh6KERRkfst5Xt3f7" />
       </section>
     </div>
   );
