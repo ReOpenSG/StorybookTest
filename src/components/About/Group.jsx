85773cec3fa1f4c8dd3b5830cb93beadc1edbc3c
ryudg <ryudg95@gmail.com>
2023-12-15 12:22:27 +0900

    🐛 [Fix] : Group 컴포넌트 제목 추가 - 기존 div 영역을 section으로 변경 후 h3 제목 추가
    Github issue #69

--- src/components/About/Group.jsx
+++ src/components/About/Group.jsx
@@ -3,14 +3,15 @@ import Title from './Common/Title';
 
 function Group() {
   return (
-    <div className="w-full flex flex-col items-center -bg--openfoundation-primary -text--openfoundation-secondary desktop:px-open-margin-desktop tablet:px-open-margin-desktop px-open-margin-mobile desktop:py-open-7xl tablet:py-open-7xl py-open-margin-desktop">
+    <section className="w-full flex flex-col items-center -bg--openfoundation-primary -text--openfoundation-secondary desktop:px-open-margin-desktop tablet:px-open-margin-desktop px-open-margin-mobile desktop:py-open-7xl tablet:py-open-7xl py-open-margin-desktop">
+      <h3 className="sr-only">그룹 소개</h3>
       <Title title="WHAT WE DO" subtitle="Open Solution Group">
         <p className="flex flex-col font-open-heading desktop:text-open-font-xxl tablet:text-open-font-xxl text-open-font-medium">
           <span>고객이 원하는 성과창출에</span>
           <span>기여 할 수 있는 최고의 파트너</span>
         </p>
       </Title>
-    </div>
+    </section>
   );
 }
 
