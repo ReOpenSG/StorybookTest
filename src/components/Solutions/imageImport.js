73ebe7401f0c493724bcb004a325a0736b27f62f
ryudg <ryudg95@gmail.com>
2023-12-18 14:46:15 +0900

    🐛 [Fix] : Func 컴포넌트 수정 - Func 컴포넌트 이미지 너비 수정 - Func 컴포넌트 더미 이미지 추가
    Github issue #92

---
+++ src/components/Solutions/imageImport.js
@@ -0,0 +1,23 @@
+import ACS from '@/assets/solutions_ACS.png';
+import OCS from '@/assets/solutions_OCS.png';
+import SCS from '@/assets/solutions_SCS.png';
+import CCS from '@/assets/solutions_CCS.png';
+import IDX from '@/assets/solutions_IDX Controller.png';
+import Map from '@/assets/solutions_Map Designer.png';
+import ACS_S from '@/assets/solutions_ACS Simulator.png';
+import SCS_S from '@/assets/solutions_SCS Simulator.png';
+import AIMS from '@/assets/solutions_AIMS.png';
+import HSMS from '@/assets/solutions_HSMS.png';
+
+export default {
+  ACS,
+  OCS,
+  SCS,
+  CCS,
+  'IDX Controller': IDX,
+  'Map Designer': Map,
+  'ACS Simulator': ACS_S,
+  'SCS Simulator': SCS_S,
+  AIMS,
+  HSMS,
+};
