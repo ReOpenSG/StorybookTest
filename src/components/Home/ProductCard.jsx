7403a6b0b1aa5a5a76dbabe2a0f7b8e6961b2937
Chi Hyun Noh <101504272+JuniorTunarr@users.noreply.github.com>
2023-12-18 12:21:02 +0900

    🌈 [Design] : hover 시 효과
    디자인 시안 상 있는 hover 효과가 누락되어 추가하였습니다.

--- src/components/Home/ProductCard.jsx
+++ src/components/Home/ProductCard.jsx
@@ -3,19 +3,18 @@ import { PropTypes } from 'prop-types';
 
 function ProductCard({ name, desc, imgUrl }) {
   return (
-    <div className="w-full flex justify-between items-center gap-open-4xl p-open-2xl tablet:p-open-4xl desktop:px-open-5xl desktop:py-open-4xl -bg--open-gray-950 rounded-[30px]">
+    <div className="w-full flex justify-between items-center gap-open-4xl p-open-2xl tablet:p-open-4xl desktop:px-open-5xl desktop:py-open-4xl -bg--open-gray-950 rounded-[30px]  border  border-transparent pointerhover:hover:-border--open-accent-accent">
       <div className="flex flex-col items-center tablet:items-start desktop:items-start gap-open-xl">
-        <h4 className="flex tablet:flex-col desktop:flex-col gap-open-sm font-open-label text-open-font-xl tablet:text-open-font-xxl desktop:text-open-font-xxxl">
-          <span>Open</span>
+        <h4 className="flex tablet:flex-col desktop:flex-col gap-open-sm font-open-label text-open-font-xxl tablet:text-open-font-xxxxl desktop:text-open-font-xxxxl">
           <span>{name}</span>
         </h4>
         <div className="flex flex-col items-center tablet:items-start desktop:items-start gap-open-lg">
-          <p className="font-open-paragraph text-open-font-medium desktop:text-open-font-large">
+          <p className="font-open-paragraph text-open-font-medium  tablet:text-open-font-large desktop:text-open-font-large">
             {desc}
           </p>
           <button
             type="button"
-            className="font-open-label text-open-font-medium desktop:text-open-font-large"
+            className="font-open-label text-open-font-small tablet:text-open-font-medium desktop:text-open-font-medium"
           >
             Learn More &gt;
           </button>
