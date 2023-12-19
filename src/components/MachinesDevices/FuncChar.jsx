c5a377a1b53e167baf0670feaab92ba5d782643f
ryudg <ryudg95@gmail.com>
2023-12-19 12:59:54 +0900

    ✨ [Feat] : FuncChar 컴포넌트 더미 아이콘 추가 - google MUI icon svg 파일로 이미지 추가
    Github issue #112

--- src/components/MachinesDevices/FuncChar.jsx
+++ src/components/MachinesDevices/FuncChar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import PropTypes from 'prop-types';
 import styles from './MachinesDevices.module.css';
+import iconMap from './imporIcon.js';
 
 function FuncChar({ descProps, selectedProduct }) {
   // console.log(descProps.types[selectedProduct]?.char);
@@ -13,6 +14,13 @@ function FuncChar({ descProps, selectedProduct }) {
           descProps.types[selectedProduct]?.char.map((item) => (
             <li key={uuidv4()} className={styles.item}>
               {/* <img src="http://via.placeholder.com/120x120" alt="Machines" /> */}
+              <img
+                src={iconMap[item[1]]}
+                alt="Machines"
+                className="desktop:w-open-5xl tablet:w-open-5xl w-open-2xl desktop:h-open-5xl tablet:h-open-5xl h-open-w-open-2xl"
+              />
+
+              {/* <div>iconMap[item[1]]</div> */}
               <span className={styles.itemText}>{item[0]}</span>
             </li>
           ))}
