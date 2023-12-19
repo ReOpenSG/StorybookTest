822c89f7e93d621ace1da264c053d1c6e2e2ce8a
Chi Hyun Noh <101504272+JuniorTunarr@users.noreply.github.com>
2023-12-19 15:57:13 +0900

    ⚙️ [Chore] : 패키지 추가
    정적 이미지 최적화를 위한 패키지를 설치하였습니다.
    (@vheemstra/vite-plugin-imagemin, imagemin-pngquant, imagemin-svgo, imagemin-webp)
    
    Related to #140

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,27 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'node:path';
+import viteImagemin from '@vheemstra/vite-plugin-imagemin';
+import imageminPngquant from 'imagemin-pngquant';
+import imageminSvgo from 'imagemin-svgo';
+import imageminWebp from 'imagemin-webp';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteImagemin({
+      plugins: {
+        png: imageminPngquant(),
+        svg: imageminSvgo(),
+      },
+      makeWebp: {
+        plugins: {
+          png: imageminWebp(),
+        },
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
