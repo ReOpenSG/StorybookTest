345731ed14f88f3f86123e236ff8dd767bb79fc2
ingbinsee <ingbinsee@gmail.com>
2023-12-19 14:49:26 +0900

    🗑️ [Deprecated] : Library 페이지에 적용한 height 값 삭제
    RootLayout에 min-height를 적용함에 따라 개별 페이지에 적용한 height 값을 삭제하였습니다.
    
    Related to: #120

--- src/views/Library.jsx
+++ src/views/Library.jsx
@@ -5,7 +5,7 @@ import DownloadSection from '@/components/Library/DownloadSection';
 function Library() {
   return (
     <div className="w-full px-open-margin-mobile tablet:px-open-margin-desktop desktop:px-open-margin-desktop">
-      <div className="max-w-[1320px] m-auto w-full h-screen">
+      <div className="max-w-[1320px] m-auto w-full">
         <LibraryTitleSection category="Support" title="라이브러리" />
         <DownloadSection />
       </div>
