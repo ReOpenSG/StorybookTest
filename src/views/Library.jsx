892454d1104ef6154cf9689510fe445d6c1edc5e
ingbinsee <ingbinsee@gmail.com>
2023-12-19 11:41:32 +0900

    ♻️ [Refactor] : Library 페이지 내용 추가, 콘텐츠 height 값 조정
    Library 페이지에 회사 소개서, 회사 브로슈어 영문을 추가하였습니다.
    데이터의 양이 적어 디바이스 높이보다 콘텐츠의 높이가 낮은 문제가 발생함에 따라 height 값을 h-screen으로 조정하였습니다.
    
    Related to: #120

--- src/views/Library.jsx
+++ src/views/Library.jsx
@@ -5,7 +5,7 @@ import DownloadSection from '@/components/Library/DownloadSection';
 function Library() {
   return (
     <div className="w-full px-open-margin-mobile tablet:px-open-margin-desktop desktop:px-open-margin-desktop">
-      <div className="max-w-[1320px] m-auto w-full">
+      <div className="max-w-[1320px] m-auto w-full h-screen">
         <LibraryTitleSection category="Support" title="라이브러리" />
         <DownloadSection />
       </div>
