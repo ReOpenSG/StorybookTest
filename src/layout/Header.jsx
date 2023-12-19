a1c46933125079a0cbdf847ffed9d2b3cdbffde6
dani-day <allkite5122@gmail.com>
2023-12-19 19:15:24 +0900

    ✨ [Feat] : location 에 따라 header 및 submenu 색상 변경 기능 추가
    + Location 주소에 따라 header 및 submenu 색상 변경 기능을 추가하였습니다.

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,17 +1,22 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, {
+  Children, useEffect, useRef, useState,
+} from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import MenuLink from '@/components/Header/MenuLink';
 import Sitemap from '@/components/Header/Sitemap';
 import SitemapMobile from '@/components/Header/SitemapMobile';
 import opensgLogo from '@/assets/header_opensg.png';
+import opensgLogoBlue from '@/assets/header_opensgBlue.png';
 import hamburgerIcon from '@/assets/header_hamburger.svg';
 import closeIcon from '@/assets/header_close.svg';
 import styles from '@/components/Header/Header.module.css';
 
 function Header() {
+  const [whiteHeader, setWhiteHeader] = useState(null);
   const [activeMenu, setActiveMenu] = useState(null);
   const [activeLanguage, setActiveLanguage] = useState(null);
   const menuRef = useRef(null);
+  const location = useLocation();
 
   const handleLanguageActive = (lang) => {
     setActiveLanguage(lang);
@@ -25,10 +30,17 @@ function Header() {
     setActiveMenu(null);
   };
 
-  const location = useLocation();
+  useEffect(() => {
+    console.log(location);
+    handleMenuInactive();
+  }, [location]);
 
   useEffect(() => {
-    handleMenuInactive();
+    if (location.pathname.includes('/solutions') || location.pathname.includes('/machines') || location.pathname.includes('/devices') || location.pathname === '/community' || location.pathname === '/library' || location.pathname === '/contact') {
+      setWhiteHeader(true);
+    } else {
+      setWhiteHeader(false);
+    }
   }, [location]);
 
   useEffect(() => {
@@ -86,7 +98,7 @@ function Header() {
   return (
     <header
       role="banner"
-      className="fixed flex justify-between items-center w-full py-open-lg px-open-margin-mobile tablet:px-open-gutter-desktop desktop:px-open-4xl -bg--openfoundation-primary -text--openfoundation-secondary font-open-font text-open-font-large font-open-paragraph h-[50px] tablet:h-[80px] desktop:h-[80px] z-20"
+      className={whiteHeader ? styles.whiteHeader : styles.header}
     >
       {activeMenu === 'Sitemap' && (
       <div className="block w-[100vw] h-[100vh] absolute top-[80px] left-0 backdrop-blur-sm">
@@ -99,14 +111,14 @@ function Header() {
       <nav className="flex">
         <ul className="flex justify-center items-center gap-open-gutter-mobile desktop:gap-open-gutter-desktop">
           <li>
-            <Link className="px-open-md py-open-sm" to="/"><img className="w-[60px] tablet:w-[100px] desktop:w-[100px]" src={opensgLogo} alt="오픈에스지" /></Link>
+            <Link className="px-open-md py-open-sm" to="/"><img className="w-[60px] tablet:w-[100px] desktop:w-[100px] mr-open-2xl" src={whiteHeader ? opensgLogoBlue : opensgLogo} alt="오픈에스지" /></Link>
           </li>
           {activeMenu !== 'Sitemap' && (
             <li className="px-open-md py-open-sm desktop:block tablet:block hidden" onMouseEnter={() => handleMenuActive('AboutUs')}>
               <button className={styles.menuLink} type="button" onClick={() => handleMenuActive('AboutUs')}>About Us</button>
               {activeMenu === 'AboutUs' && (
               <div className="absolute left-0 top-[80px] w-full backdrop-blur-[2px]" ref={menuRef} onMouseLeave={handleMenuInactive}>
-                <ul className="flex flex-col gap-open-md py-open-xl -bg--openfoundation-primary opacity-80 tablet:pl-[152px] desktop:pl-[186px]">
+                <ul className={whiteHeader ? styles.aboutMenuListWhite : styles.aboutMenuList}>
                   <MenuLink linkName="회사개요" linkAddress="/about" />
                   <MenuLink linkName="연혁" linkAddress="/history" />
                   <MenuLink linkName="직무소개" linkAddress="/team" />
@@ -121,13 +133,15 @@ function Header() {
             <button className={styles.menuLink} type="button" onClick={() => handleMenuActive('Products')}>Products</button>
             {activeMenu === 'Products' && (
             <div className="absolute left-0 top-[80px] w-full backdrop-blur-[2px]" ref={menuRef} onMouseLeave={handleMenuInactive}>
-              <ul className="flex flex-row gap-open-xl px-open-xl py-open-xl text-open-font-large font-open-label -bg--openfoundation-primary opacity-80 tablet:pl-[268px] desktop:pl-[322px]">
+              <ul className={whiteHeader ? styles.productMenuListWhite : styles.productMenuList}>
                 <li>
                   Solutions
                   <ul className="font-open-paragraph flex flex-col pt-open-lg gap-open-lg">
-                    <li className="text-[#3FA9F5] text-open-font-medium ">
-                      Equipment Control System
-                      <ul className="flex flex-col pl-open-sm pt-open-md gap-open-md text-open-font-medium -text--openfoundation-secondary">
+                    <li className="text-open-font-medium ">
+                      <div className="-text--open-accent-accent-1">
+                        Equipment Control System
+                      </div>
+                      <ul className="flex flex-col pl-open-sm pt-open-md gap-open-md text-open-font-medium ">
                         <MenuLink linkName="ACS/FMS" tooltip="AGV Control System / Fleet Management System" linkAddress="solutions/ACS" />
                         <MenuLink linkName="OCS" tooltip="OHT Control System" linkAddress="solutions/OCS" />
                         <MenuLink linkName="SCS" tooltip="Stocker Control System" linkAddress="solutions/SCS" />
@@ -135,23 +149,29 @@ function Header() {
                         <MenuLink linkName="IDX Controller" tooltip="Index Control System" linkAddress="solutions/IDX Controller" />
                       </ul>
                     </li>
-                    <li className="text-[#3FA9F5] text-open-font-medium ">
-                      Smart Tools
-                      <ul className="flex flex-col pl-open-sm pt-open-md gap-open-md text-open-font-medium -text--openfoundation-secondary">
+                    <li className="text-open-font-medium ">
+                      <div className="-text--open-accent-accent-1">
+                        Smart Tools
+                      </div>
+                      <ul className="flex flex-col pl-open-sm pt-open-md gap-open-md text-open-font-medium  ">
                         <MenuLink linkName="Map Designer" linkAddress="solutions/Map Designer" />
                         <MenuLink linkName="ACS Simulator" tooltip="AGV Control System Simulator" linkAddress="solutions/ACS Simulator" />
                         <MenuLink linkName="SCS Simulator" tooltip="Stocker Control System Simulator" linkAddress="solutions/SCS Simulator" />
                       </ul>
                     </li>
-                    <li className="text-[#3FA9F5] text-open-font-medium ">
-                      Monitoring Solution
-                      <ul className="flex flex-col pl-open-sm pt-open-md gap-open-md text-open-font-medium -text--openfoundation-secondary">
+                    <li className="text-open-font-medium ">
+                      <div className="-text--open-accent-accent-1 -text-">
+                        Monitoring Solution
+                      </div>
+                      <ul className="flex flex-col pl-open-sm pt-open-md gap-open-md text-open-font-medium ">
                         <MenuLink linkName="AIMS" tooltip="Advanced Integrated Monitoring Solution" linkAddress="solutions/AIMS" />
                       </ul>
                     </li>
-                    <li className="text-[#3FA9F5] text-open-font-medium ">
-                      Comm Driver
-                      <ul className="flex flex-col pl-open-sm pt-open-md gap-open-md text-open-font-medium -text--openfoundation-secondary">
+                    <li className="text-open-font-medium ">
+                      <div className="-text--open-accent-accent-1">
+                        Comm Driver
+                      </div>
+                      <ul className="flex flex-col pl-open-sm pt-open-md gap-open-md text-open-font-medium ">
                         <MenuLink linkName="Open HSMS" linkAddress="solutions/HSMS" />
                       </ul>
                     </li>
@@ -159,7 +179,7 @@ function Header() {
                 </li>
                 <li>
                   Smart Machine
-                  <ul className="flex flex-col pl-open-sm pt-open-md gap-open-md text-open-font-medium font-open-paragraph">
+                  <ul className="flex flex-col pl-open-sm pt-open-md gap-open-md text-open-font-medium font-open-paragraph ">
                     <MenuLink linkName="AGV - Lift Type1" linkAddress="/machines/Lift Type1" />
                     <MenuLink linkName="AGV - Lift Type2" linkAddress="/machines/Lift Type2" />
                     <MenuLink linkName="AGV - Fork-Lift Type" linkAddress="/machines/Fork-Lift Type" />
@@ -168,14 +188,16 @@ function Header() {
                 </li>
                 <li>
                   Smart Device
-                  <ul className="flex flex-col pl-open-sm pt-open-md gap-open-md text-open-font-medium font-open-paragraph">
+                  <ul className="flex flex-col pl-open-sm pt-open-md gap-open-md text-open-font-medium font-open-paragraph ">
                     <MenuLink linkName="Microsoft Hololens 2" linkAddress="/devices/Microsoft" />
                   </ul>
                 </li>
                 <div className="w-[1px] -bg--openfoundation-secondary" />
-                <li className="text-[#FFE500]">
-                  Industries
-                  <ul className="flex flex-col items-center pl-open-sm pt-open-md gap-open-md text-open-font-medium font-open-paragraph -text--openfoundation-secondary">
+                <li>
+                  <div className="-text--open-accent-accent-1">
+                    Industries
+                  </div>
+                  <ul className="flex flex-col pl-open-sm pt-open-md gap-open-md text-open-font-medium font-open-paragraph ">
                     <MenuLink linkName="반도체" linkAddress="/industries/semiconductor" />
                     <MenuLink linkName="디스플레이" linkAddress="/industries/display" />
                     <MenuLink linkName="2차전지" linkAddress="/industries/battery" />
@@ -192,7 +214,7 @@ function Header() {
             <button className={styles.menuLink} type="button" onClick={() => handleMenuActive('Support')}>Support</button>
             {activeMenu === 'Support' && (
             <div className="absolute left-0 top-[80px] w-full backdrop-blur-[2px]" ref={menuRef} onMouseLeave={handleMenuInactive}>
-              <ul className="flex flex-col gap-open-md px-open-xl py-open-xl text-open-font-large -bg--openfoundation-primary opacity-80 tablet:pl-[364px] desktop:pl-[440px]">
+              <ul className={whiteHeader ? styles.supportMenuListWhite : styles.supportMenuList}>
                 <MenuLink linkName="커뮤니티" linkAddress="/community" />
                 <MenuLink linkName="라이브러리" linkAddress="/library" />
                 <MenuLink linkName="고객문의" linkAddress="/contact" />
@@ -206,9 +228,9 @@ function Header() {
       <ul className="flex justify-center items-center gap-open-gutter-mobile tablet:gap-open-gutter-desktop desktop:gap-open-gutter-desktop">
         <li className="flex justify-center items-center px-open-md py-open-sm text-open-font-small desktop:text-open-font-medium tablet:text-open-font-medium">
           <div className="flex flex-row gap-open-md">
-            <button className={activeLanguage !== 'ENG' ? '-text--openfoundation-secondary' : '-text--openfoundation-tertiary'} type="button" onClick={() => handleLanguageActive('KOR')}>KOR</button>
+            <button className={activeLanguage !== 'ENG' ? '' : '-text--openfoundation-tertiary'} type="button" onClick={() => handleLanguageActive('KOR')}>KOR</button>
             <div className="w-[1px] -bg--openfoundation-tertiary" />
-            <button className={activeLanguage === 'ENG' ? '-text--openfoundation-secondary' : '-text--openfoundation-tertiary'} type="button" onClick={() => handleLanguageActive('ENG')}>ENG</button>
+            <button className={activeLanguage === 'ENG' ? '' : '-text--openfoundation-tertiary'} type="button" onClick={() => handleLanguageActive('ENG')}>ENG</button>
           </div>
         </li>
         <li className="flex justify-center items-center">
