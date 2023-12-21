a88acf09d38522981693531db02c71018ee45f49
Chi Hyun Noh <101504272+JuniorTunarr@users.noreply.github.com>
2023-12-21 11:42:04 +0900

    🌈 [Design] : 메인페이지 - 슬로건 배경이미지 배치
    슬로건 컴포넌트에 배경 이미지를 추가하고, 레이아웃을 수정했습니다.

--- src/components/Home/Slogan.jsx
+++ src/components/Home/Slogan.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import styles from './Home.module.css';
+import BgImg from '@/assets/home_slogan.svg';
 
 function Slogan() {
   return (
     <section className={styles.slogan}>
-      <div className={styles.wrapper}>
-        <h3 className={styles.title}>
-          OPEN Your Future
-          <br />
-          with&nbsp;
-          <span className={styles.gradient}>OpenSG</span>
-        </h3>
-        <small className={styles.subtitle}>
-          고객에게 열려있는 기업, 고객의 미래를 여는 기업
-          <br />
-          OpenSG 가 고객과 함께 미래로 가는 길을 엽니다
-        </small>
+      <div style={{ backgroundImage: `url('${BgImg}')` }} className={styles.bgWrapper}>
+        <div className={styles.wrapper}>
+          <h3 className={styles.title}>
+            OPEN Your Future
+            <br />
+            with&nbsp;
+            <span className={styles.gradient}>OpenSG</span>
+          </h3>
+          <small className={styles.subtitle}>
+            고객에게 열려있는 기업, 고객의 미래를 여는 기업
+            <br />
+            OpenSG 가 고객과 함께 미래로 가는 길을 엽니다
+          </small>
+        </div>
       </div>
     </section>
   );
