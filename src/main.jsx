794684ba8cd58693e822a3a43532bbd1bab222ce
Chi Hyun Noh <101504272+JuniorTunarr@users.noreply.github.com>
2023-12-21 15:47:22 +0900

    🏛️ [Style] : helmetProvider 위치 변경
    helmetProvider는 App 컴포넌트를 감싸야하는데,
    main.jsx에서 routerProvider를 감싸고있어 수정하였습니다.

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
+import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
 import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RecoilRoot>
-      <App />
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
     </RecoilRoot>
   </React.StrictMode>,
 );
